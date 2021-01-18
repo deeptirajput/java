924fd307aa1053b27bfba4fc4d593bdea4a18a0b
DeeptiRajput <deepti.rajput@publicissapient.com>
2021-01-18 14:05:37 +0530

    Added sample for learning git

---
+++ tutorial/src/main/java/collectTutorial/Sample.java
@@ -0,0 +1,12 @@
+package collectTutorial;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Sample{
+
+    public static void main(String[] args) {
+     System.out.println("Hello world");
+    }
+}
+
