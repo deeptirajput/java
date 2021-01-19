63e442d6ec869a0d44cdbaaa61354e3be434e840
DeeptiRajput <deepti.rajput@publicissapient.com>
2021-01-19 17:23:48 +0530

    Reset demo commit1

--- tutorial/src/main/java/collectTutorial/Sample.java
+++ tutorial/src/main/java/collectTutorial/Sample.java
@@ -7,7 +7,7 @@ public class Sample{
 
     public static void main(String[] args) {
      System.out.println("Hello world");
-	 System.out.println("git ammend demo");
+	 System.out.println("git reset demo");
     }
 }
 
